7a4d3805a1d266448ab731a5be2e3ce4464f2308
Pepijn te Winkel <pepi@fedora.fritz.box>
2026-02-06 13:51:00 +0100

    added basic item list

--- public/index.php
+++ public/index.php
@@ -6,9 +6,9 @@ include "./includes/nav.php";
     <img id="hero_img" src="./img/site/hero.jpg" alt="test">
     <section class="section" id="promotion">
         <div class="content_wrapper" id="promotion_inner">
-            <p id="collection_subtitle">subtitle</p>
-            <h2 id="collection_title">title</h2>
-            <p id="collection_description">description</p>
+            <p class="subtitle">subtitle</p>
+            <h2 class="title">title</h2>
+            <p class="description">description</p>
             <a class="button" id="collection_button" href="#">Shop</a>
         </div>
     </section>
@@ -18,10 +18,23 @@ include "./includes/nav.php";
             <img class="soon_available_img" src="./img/site/hero.jpg" alt="test">
         </div>
         <div class="content_wrapper" id="soon_available_inner">
-            <p id="soon_available_subtitle">Binnenkort verkijgbaar</p>
+            <p class="subtitle">Binnenkort verkijgbaar</p>
             <h2 id="soon_available_title">Collection Name</h2>
             <a class="button" id="soon_available_button" href="#">Shop</a>
         </div>
+    </section>
+    <section class="section" id="featured">
+        <div class="content_wrapper" id="featured_inner">
+            <p>Featured now</p>
+            <div id="featured_photos">
+                <img class="featured_img" src="./img/site/hero.jpg" alt="test">
+                <img class="featured_img" src="./img/site/hero.jpg" alt="test">
+                <img class="featured_img" src="./img/site/hero.jpg" alt="test">
+            </div>
+        </div>
+    </section>
+    <section class="section" id="products">
+        <div class="content_wrapper" id="products_inner"></div>
     </section>
 </main>
 <?php include "./includes/footer.php"; ?>
---
+++ public/products.php
@@ -0,0 +1,44 @@
+<?php
+include "./includes/header.php";
+include "./includes/nav.php";
+?>
+<main>
+    <section class="section" id="shop">
+        <div class="content_wrapper" id="shop_inner">
+            <div id="shop_sidebar">Filters</div>
+            <div id="shop_content">
+                <div class="shop_item">
+                    <img class="shop_item_img" src="./img/site/hero.jpg" alt="test">
+                    <p>Name</p>
+                    <p>Catogory</p>
+                    <p>Price</p>
+                </div>
+                <div class="shop_item">
+                    <img class="shop_item_img" src="./img/site/hero.jpg" alt="test">
+                    <p>Name</p>
+                    <p>Catogory</p>
+                    <p>Price</p>
+                </div>
+                <div class="shop_item">
+                    <img class="shop_item_img" src="./img/site/hero.jpg" alt="test">
+                    <p>Name</p>
+                    <p>Catogory</p>
+                    <p>Price</p>
+                </div>
+                <div class="shop_item">
+                    <img class="shop_item_img" src="./img/site/hero.jpg" alt="test">
+                    <p>Name</p>
+                    <p>Catogory</p>
+                    <p>Price</p>
+                </div>
+                <div class="shop_item">
+                    <img class="shop_item_img" src="./img/site/hero.jpg" alt="test">
+                    <p>Name</p>
+                    <p>Catogory</p>
+                    <p>Price</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php include "./includes/footer.php"; ?>
