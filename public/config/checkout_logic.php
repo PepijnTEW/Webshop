8e4c81a6a4d32aaea202a748c70420ddfb39eee1
Pepijn <pep@fedora.fritz.box>
2026-02-17 12:12:01 +0100

    added basic insert into order info it is still missing the order content

--- public/config/checkout_logic.php
+++ public/config/checkout_logic.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . "/db_connect.php";
 
+$formdata = [];
 $fields = [
     'aanhef',
     'voornaam',
@@ -15,8 +16,25 @@ $fields = [
     'land',
 ];
 if (isset($_POST['voorwaarden'])){
-    $formdata = [];
     foreach ($fields as $field) {
         $formdata[$field] = $_POST[$field];
     }
-} else
+    $columns = implode(",", $fields);
+    $placeholders = implode(",", array_fill(0, count($fields), "?"));
+    $sql = "INSERT INTO orders ($columns) VALUES ($placeholders)";
+    $stmt = $mysqli->prepare($sql);
+
+    $types = str_repeat("s", count($fields));
+    $params = array_values($formdata);
+
+    $stmt->bind_param($types, ...$params);
+    $stmt->execute();
+
+    $insertedId = $mysqli->insert_id;
+
+    $stmt->close();
+    $mysqli->close();
+
+} else {
+    echo "f u";
+};
